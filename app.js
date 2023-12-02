b5df67bcd7a0e1354f73d36cd6dfa0c5de602bf2
kndavenpo <122413804+kndavenpo@users.noreply.github.com>
2023-12-02 11:38:21 -0500

    CS 5610: Home and profile screens

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-import express from 'express'
-import Hello from "./hello.js"
-const app = express()
+import "dotenv/config";
+import express from 'express';
+import Hello from "./hello.js";
+import cors from "cors";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
 Hello(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
