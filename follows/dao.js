df85239ccff47a5786d5a2816686ba0bd0374250
kndavenpo <122413804+kndavenpo@users.noreply.github.com>
2023-12-09 21:33:19 -0500

    follows function

--- follows/dao.js
+++ follows/dao.js
@@ -6,7 +6,11 @@ export const userFollowsUser = (follower, followed) =>
     model.create({ follower, followed });
 export const userUnfollowsUser = (follower, followed) =>
     model.deleteOne({ follower, followed });
+
+// POPULATE IS THE - RESPOND WITH THE DOCUMENT
 export const findFollowersOfUser = (followed) =>
     model.find({ followed }).populate("follower");
+
+// POPULATE IS THE JOIN - RESPOND WITH THE DOCUMENT
 export const findFollowedUsersByUser = (follower) =>
     model.find({ follower }).populate("followed");
